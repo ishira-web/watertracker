6799159aba04997b09317d887293b3aab589d2a3
ishira-web <ishira.pahasara@icloud.com>
2025-11-30 01:05:30 +0530

    latest

--- water-tracker-server/src/server.ts
+++ water-tracker-server/src/server.ts
@@ -11,7 +11,7 @@ dotenv.config();
 connectDB();
 
 const app: Application = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const CLIENT_URL = process.env.CLIENT_URL ;
 
